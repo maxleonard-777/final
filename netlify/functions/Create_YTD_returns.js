e76159f8b45d2a96e1fa2eed0546f5cbe6fe9f84
Max Leonard <max.leonard@kellogg.northwestern.edu>
2021-06-02 17:41:19 -0500

    create holdings and create ytd returns

---
+++ netlify/functions/Create_YTD_returns.js
@@ -0,0 +1,2 @@
+// Take the holdings as they are in firebase, and the holdings you've created, and calculate YTD returns
+// and add these to a value that the HTML can use to display 
---
+++ netlify/functions/create_holding.js
@@ -0,0 +1,4 @@
+// Goal: Provide a function to create a new holding in Firebase
+
+// allows us to use firebase
+let firebase = require(`./firebase`)
