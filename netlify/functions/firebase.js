a4a14e8e823238212fe803f78519cb03576f8acd
Your Name <you@example.com>
2021-06-08 19:51:45 -0500

    jc edit

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,12 +1,12 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {apiKey: "AIzaSyCoyimqwfmTFwpKyT2dMLqo3Z4BExwswHg",
-authDomain: "kiei-451-6042a.firebaseapp.com",
-projectId: "kiei-451-6042a",
-storageBucket: "kiei-451-6042a.appspot.com",
-messagingSenderId: "490679380429",
-appId: "1:490679380429:web:a5db203b4f6bcb27df7c84"} // replace
+const firebaseConfig = {apiKey: "AIzaSyBxfAHgf-3xzlllIXwU-BnqdtcbuUHpn2Q",
+authDomain: "kiei-451-4a4a6.firebaseapp.com",
+projectId: "kiei-451-4a4a6",
+storageBucket: "kiei-451-4a4a6.appspot.com",
+messagingSenderId: "184245553330",
+appId: "1:184245553330:web:299f2b57c61d5265d461ab"} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
